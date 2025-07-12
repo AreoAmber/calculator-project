382056ea99ba23a772748bc142f230fd04d57110
AreoAmber <areorolley@gmail.com>
2025-07-12 14:26:01 +0200

    Calculator UI & Bug Fixes completed ui buttons with operators and digits on them

--- script.js
+++ script.js
@@ -11,5 +11,20 @@ function divide(a, b) {
 }
 
 function multiply(a, b) {
-  a * b;
+  return a * b;
 }
+
+// Calculator operation
+const numberOne = 3;
+const operation = "+";
+const numberTwo = 6;
+
+function operate(a, b, operation) {
+  const a = 4;
+  const b = 7;
+  operation = add();
+
+  return `${a}, ${operation}, ${b}`;
+}
+
+console.log(operate);
